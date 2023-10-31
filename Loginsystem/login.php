77b1eec71244900cd703508e760bf97cc0bfc647
Andre Thomas Castro <125794581+adrcstro@users.noreply.github.com>
2023-10-31 11:41:02 +0800

    frontned login system
    frontned login system

--- Loginsystem/login.php
+++ Loginsystem/login.php
@@ -3,18 +3,205 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="x-icon" href="images/webicon.png">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="shortcut icon" type="x-icon" href="../images/webicon.png">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login.css">
    
     <title>Brangay409-3WHeel-Tracking-System</title>
 </head>
 
 <body>
-<h1>hello andre</h1>
+
+<main>
+      <div class="box">
+        <div class="inner-box">
+          <div class="forms-wrap">
+            <form action="index.html" autocomplete="off" class="sign-in-form">
+  
+<script>
+    function changeLoginType(type) {
+        document.getElementById('loginDropdown').innerText = 'Login as ' + type;
+    }
+</script>
+              <div class="heading">
+                <h2>Welcome!</h2>
+                <h6>Not registred yet?</h6>
+                <a href="#" class="toggle" data-target="img-2">Sign in</a>
+              </div>
+              <div class="dropdown">
+    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="loginDropdown">
+        Login as
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="#" onclick="changeLoginType('Passenger')">Passenger</a></li>
+        <li><a class="dropdown-item" href="#" onclick="changeLoginType('Driver')">Driver</a></li>
+        <li><a class="dropdown-item" href="#" onclick="changeLoginType('Admin')">Admin</a></li>
+    </ul>
+</div>
+              <div class="actual-form">
+                <div class="input-wrap">
+                  <input
+                    type="text"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Email</label>
+                </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="password"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Password</label>
+                </div>
+                <p class="text">
+                  Forgotten your password on your login datails?
+
+                  <a href="Forgotpassword.php" data-target="img-3">Get help signing in</a>
+                </p>
+                <input type="submit" value="Sign In" class="sign-btn" />
+                <h5 id="textcon">Continue with</h5>
+                <button type="button" class="login-with-google-btn" >
+                Sign in with Google
+                 </button>
+              </div>
+            </form>
+
+
+            <form action="index.html" autocomplete="off" class="sign-up-form">
+       
+              <div class="heading">
+            
+
+                <h2>Get Started</h2>
+                <h6>Already have an account?</h6>
+                <a href="#" class="toggle" data-target="img-1">Sign up</a>
+              </div>
+              <div class="dropdown">
+    <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" id="loginDropdown">
+        Signup as
+    </button>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="Passenger.php" onclick="changeLoginType('Passenger')">Passenger</a></li>
+        <li><a class="dropdown-item" href="Driverlogin.php" onclick="changeLoginType('Driver')">Driver</a></li>
+    </ul>
+</div>
+              <div class="actual-form">
+                <div class="input-wrap">
+                  <input
+                    type="text"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Name</label>
+                </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="email"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Email</label>
+                </div>
+
+                <div class="input-wrap">
+                  <input
+                    type="password"
+                    minlength="4"
+                    class="input-field"
+                    autocomplete="off"
+                    required
+                  />
+                  <label>Password</label>
+                </div>
+                <p class="text">
+                  By signing up, I agree to the
+                  <a href="#">Terms of Services</a> and
+                  <a href="#">Privacy Policy</a>
+                </p>
+                <input type="submit" value="Sign Up" class="sign-btn" />
+                <div class="logtext">   
+                <h5 id="textcon">Continue with</h5>
+                <button type="button" class="login-with-google-btn" >
+                Sign in with Google
+                 </button>
+                 </div>
+              </div>
+            </form>
+
+
+
+          </div>
+
+          <div class="carousel">
+  <div class="images-wrapper">
+    <img src="../images/login.svg" class="image img-1 show" data-image="img-1" alt="" />
+    <img src="../images/Signup.svg" class="image img-2" data-image="img-2" alt="" />
+    <img src="../images/Forgot.svg" class="image img-3" data-image="img-3" alt="" />
+  </div>
+
+  <div class="text-slider">
+    <div class="text-wrap">
+      <div class="text-group">
+        <h2>Get Started</h2>
+        <h2>Join us</h2>
+        <h2>Set new Password</h2>
+      </div>
+    </div>
+
+    <div class="bullets">
+      <span class="active" id="login" data-value="1"></span>
+     
+    </div>
+  </div>
+</div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const toggles = document.querySelectorAll(".toggle");
+
+    toggles.forEach(function (toggle) {
+      toggle.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = toggle.getAttribute("data-target");
+        const images = document.querySelectorAll(".image");
+        images.forEach(function (image) {
+          if (image.classList.contains(target)) {
+            image.classList.add("show");
+          } else {
+            image.classList.remove("show");
+          }
+        });
+      });
+    });
+  });
+ 
+</script>
+
+
+
+
+
+          
+        </div>
+      </div>
+    </main>
+
+
+      
 </body>
-<script src="js/bootstrap.bundle.js"></script>
-    <script src="script.js"></script>
+<script src="../js/bootstrap.bundle.js"></script>
+    <script src="login.js"></script>
 
 </html>
